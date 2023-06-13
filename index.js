59555aad04e84eded68b272abf5e9675780b8ba9
SIBO BALDE <bussinesskolding@gmail.com>
2023-06-13 19:38:53 +0100

    cors

--- index.js
+++ index.js
@@ -40,7 +40,12 @@ dotenv.config()
        // the express app
 
 const app = express();
-app.use(cors())
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.get('/api/keys/paypal', (req, res)=> {
